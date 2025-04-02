4bb492a6c77967ac8ebfd36b774e5b590a55c736
luiz.martinez <luizantmartinezfilho3@gmail..com>
2025-04-02 11:46:51 -0300

    Exercicios completos

---
+++ Calculadora.cs
@@ -0,0 +1,43 @@
+﻿namespace Exercicios;
+internal class Calculadora
+{
+    private void Soma(double n1, double n2)
+    {
+        Console.WriteLine("Soma: " + (n1 + n2));
+    }
+
+    private void Substracao(double n1, double n2)
+    {
+        Console.WriteLine("Substração: " + (n1 - n2));
+    }
+
+    private void Multiplicacao(double n1, double n2)
+    {
+        Console.WriteLine("Multiplicação: " + (n1 * n2));
+    }
+
+    private void Divisao(double n1, double n2)
+    {
+        if(n2 == 0)
+        {
+            Console.WriteLine("Não é possível dividir por 0");
+        } else
+        {
+            Console.WriteLine("Divisão: " + (n1 / n2));
+        }
+    }
+
+    private void Media(double n1, double n2)
+    {
+        Console.WriteLine("Média: " + ((n1 + n2) / 2));
+    }
+
+    public void ExibirContas(double n1, double n2)
+    {
+        Soma(n1, n2);
+        Substracao(n1, n2);
+        Multiplicacao(n1, n2);
+        Divisao(n1, n2);
+        Media(n1, n2);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,70 @@
-﻿
+﻿// 1
+
+/* Console.WriteLine("Digite seu nome:");
+string? nome = Console.ReadLine();
+
+Console.WriteLine($"Olá, {nome}! Seja muito bem vindo!") */
+
+
+// 2
+
+/* using System.Text;
+
+Console.WriteLine("Digite seu nome: ");
+string? nome = Console.ReadLine();
+
+Console.WriteLine("Digite seu sobrenome: ");
+string? sobrenome = Console.ReadLine();
+
+StringBuilder sb = new StringBuilder();
+sb.Append(nome);
+sb.Append(" ");
+sb.Append(sobrenome);
+
+Console.WriteLine(sb.ToString()); */
+
+
+// 3
+
+/* using Exercicios;
+
+Calculadora c = new Calculadora();
+c.ExibirContas(4, 2); */
+
+
+// 4
+
+/* Console.WriteLine("Digite alguma(s) palavra(s)");
+string? resultado = Console.ReadLine();
+
+Console.WriteLine("Número de caracteres: " + resultado.Length); */
+
+
+//5
+
+/* using System.Text.RegularExpressions;
+
+Console.WriteLine("Digite a sua placa: ");
+string? Placa = Console.ReadLine();
+
+string pattern = @"^[a-zA-Z]{3}-?\d{4}$";
+
+Regex regex = new Regex(pattern);
+
+if (regex.IsMatch(Placa))
+{
+    Console.WriteLine("Placa válida!");
+} else
+{
+    Console.WriteLine("Placa inválida!");
+} */
+
+
+// 6
+
+/* DateTime diaAtual =  DateTime.UtcNow;
+
+Console.WriteLine(diaAtual.ToLongDateString() + " "  + diaAtual.ToLongTimeString());
+Console.WriteLine(diaAtual.ToShortDateString());
+Console.WriteLine(diaAtual.Hour + " horas");
+Console.WriteLine(diaAtual.ToLongDateString()); */
